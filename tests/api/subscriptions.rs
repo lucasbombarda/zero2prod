53b1eacbf6035dc57b05ebe2bce531ab78b28a8c
Lucas Bombarda <lucas@devdock.com.br>
2025-04-24 13:36:19 -0300

    Chapter 8

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -122,7 +122,7 @@ async fn subscribe_fails_if_there_is_a_fatal_database_error() {
     let app = spawn_app().await;
     let body = "name=lucas&email=lucas%40devdock.com.br";
 
-    sqlx::query!("ALTER TABLE subscription_tokens DROP COLUMN subscription_token")
+    sqlx::query!(r#"ALTER TABLE subscriptions DROP COLUMN email;"#)
         .execute(&app.db_pool)
         .await
         .expect("Failed to drop column");
